99a21186989032fc897297788fef36ff73f6f772
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-05-02 00:23:57 -0300

    react router dom

---
+++ tiendadedeportes-app/src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const { Link } from "react-router-dom";
+const navBar=() =>{
+    return(
+        <div>
+            <li>
+                <link to="/">Nosotros</link>
+            </li>
+        </div>
+    )
+}
