119ab6595469927a947d0eea1ff14289ad685c92
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-05-02 02:02:09 -0300

    navbar

--- tiendadedeportes-app/src/App.js
+++ tiendadedeportes-app/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import NavBar from './components/navbar';
 
 function App() {
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+  const styles = {
+    color: "red"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
     </div>
   );
 }
--- tiendadedeportes-app/src/components/navbar.js
+++ tiendadedeportes-app/src/components/navbar.js
@@ -1,12 +1,32 @@
 import React from 'react';
-const {Link} from: "react-router-dom";
-const navBar=() =>{
+import {NavLink as Link} from 'react-router-dom';
+import styled from 'styled-components';
+function NavBar(){
+    const Nav = styled.nav`
+        height: 80;
+        display: flex;
+        justify-content: space-evenly;
+        padding: 0.5rem;
+    `;
+    const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: RGB(40, 42, 46);
+  `;
+  const h2 = styled.h2`
+  font-size: 1em;
+  text-align: center;
+  color: RGB(40, 42, 46);
+  `
+  
     return(
-        <div>
-            <li>
-                <link to="/">Nosotros</link>
-            </li>
-        </div>
+        <Nav>
+            <Title>Mi Deporte</Title>
+            <Title>Inicio</Title>
+            <Title>Nosotros</Title>
+            <Title>Ofertas</Title>
+            <Title>FAQ</Title>
+        </Nav>
     )
 }
-export default navBar;
+export default NavBar
