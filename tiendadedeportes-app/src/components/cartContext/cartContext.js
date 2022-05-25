40ed8120c87ffae9c38757d8a4744f5ef37e2be4
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-05-25 16:32:00 -0300

    cartContext3

--- tiendadedeportes-app/src/components/cartContext/cartContext.js
+++ tiendadedeportes-app/src/components/cartContext/cartContext.js
@@ -4,13 +4,14 @@ export const CartContext = createContext()
 export const CartProvider = ({children}) =>{
     const [items, setItems] = useState(initialState)
     const cartAdd = (detail, number) => {
-        const exist = items.find(x => x.id === detail.id)
+        const exist = items.some(x => x.id === detail.id)
         if (exist) {
             setItems(items.map(x => x.id === detail.id ? {...exist, qty: exist.qty + number} : x ))
         } else {
-            setItems([...items, {...detail, qty: number}])
+            const newItem = {...detail, qty:number}
+            setItems([...items, newItem])
+            console.log(newItem)
         }
-        console.log(items)
     }
     return(
         <CartContext.Provider value={cartAdd}>
